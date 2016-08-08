5f15441d89ec1e4eeb0dcc5b9e7310999bd41a3b
Stefan Daschek <stefan@die-antwort.eu>
2016-08-08 13:10:28 +0200

    Improve feedback and error messages.
    Two changes:
      * When running RuboCop explicitly, do not display the tooltip with the number of detected offenses (detailed output is available in the output window anyway).
      * When running silently (on save) and the RuboCop executable could not be found, show a short error message in the tooltip (the alert dialog is too intrusive for this case).

--- Support/lib/rubocop_runner.rb
+++ Support/lib/rubocop_runner.rb
@@ -8,11 +8,16 @@ class RubocopRunner
     Dir.chdir(ENV['TM_PROJECT_DIRECTORY'])
   end
 
+  # Run RuboCop for the given single file or Array of files.
+  #
+  # If a block is given, it will be passed the number detected offenses (or false if no rubocop executable was found).
   def run(file_or_files)
     return unless file_or_files
     files = Array(file_or_files)
     executable, options = find_rubocop_executable
-    if executable
+    if executable.nil?
+      yield false if block_given?
+    else
       options = {
         :script_args => %w(--format clang),
         :verb => 'Linting',
@@ -20,24 +25,21 @@ class RubocopRunner
         :use_hashbang => false,
         :version_replace => 'RuboCop \1'
       }.merge(options)
+      detected_offenses = nil
       TextMate::Executor.run(executable, files, options) do |line, _type|
-        if line =~ /(\d+) offenses? detected/
-          TextMate::UI.tool_tip(Regexp.last_match(0)) if Regexp.last_match(1).to_i > 0
-        end
+        detected_offenses = $1.to_i if line =~ /(\d+|no) offenses? detected/
         nil # Always return nil so that TextMate::Executor parses the output, too.
       end
-    else
-      buttons = ['OK', 'Open Installation Instructions']
-      btn = TextMate::UI.alert(:critical, 'Rubocop not found', 'You need to install RuboCop first.', *buttons)
-      system('open "http://rubocop.readthedocs.io/en/latest/installation/"') if btn == buttons[1]
+      yield detected_offenses if block_given?
     end
   end
 
-  def run_in_background(file_or_files)
+  # Like `#run`, but RuboCop will be run in the background (detached) to avoid blocking TextMate’s UI.
+  def run_in_background(file_or_files, &block)
     pid = fork do
       STDOUT.reopen(open('/dev/null', 'w'))
       STDERR.reopen(open('/dev/null', 'w'))
-      run(file_or_files)
+      run(file_or_files, &block)
     end
     Process.detach(pid)
   end
