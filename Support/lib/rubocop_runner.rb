cad0e5ad99a84ff58583729e1b718725a0bc0c5a
Stefan Daschek <stefan@daschek.net>
2016-08-18 10:47:09 +0200

    Use `Executable.find` from Ruby bundle.

--- Support/lib/rubocop_runner.rb
+++ Support/lib/rubocop_runner.rb
@@ -1,5 +1,6 @@
 require "#{ENV['TM_SUPPORT_PATH']}/lib/tm/executor"
 require "#{ENV['TM_SUPPORT_PATH']}/lib/ui"
+require "#{ENV['TM_RUBY_BUNDLE_SUPPORT']}/lib/executable"
 
 class RubocopRunner
   attr_reader :base_dir
@@ -19,7 +20,7 @@ class RubocopRunner
   # false if the rubocop executable could not be found).
   def run(file_or_files, script_args = [])
     files = Array(file_or_files)
-    executable, options = find_rubocop_executable
+    executable = Executable.find('rubocop')
     if executable.nil?
       yield false if block_given?
     else
@@ -34,7 +35,8 @@ class RubocopRunner
           end,
         :use_hashbang => false,
         :version_replace => 'RuboCop \1',
-      }.merge(options)
+      }
+      options[:version_args] = %w(exec rubocop --version) if executable == %w(bundle exec rubocop)
       detected_offenses = nil
       TextMate::Executor.run(executable, files, options) do |line, _type|
         detected_offenses = $1.to_i if line =~ /(\d+|no) offenses? detected/
@@ -53,17 +55,5 @@ class RubocopRunner
       run(file_or_files, script_args, &block)
     end
     Process.detach(pid)
-  end
-
-  private
-
-  def find_rubocop_executable
-    if File.exist?('bin/rubocop')
-      ['bin/rubocop', {}]
-    elsif File.exist?('Gemfile.lock') && !File.readlines('Gemfile.lock').grep(/^    rubocop/).empty?
-      [%w(bundle exec rubocop), {:version_args => %w(exec rubocop --version)}]
-    elsif system('which -s rbenv && rbenv which rubocop &>/dev/null')
-      ['rubocop', {}]
-    end
   end
 end
